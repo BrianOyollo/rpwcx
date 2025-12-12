33e83ba8c0cc2b372994bd495418428e697567b8
oyollobrian <oyollobrian@gmail.com>
2025-12-12 14:13:57 +0300

    improved profile page

--- src/streamlit/account_pages/profile.py
+++ src/streamlit/account_pages/profile.py
@@ -15,28 +15,49 @@ user_info = conn.query(
 # user_info
 user_data = user_info.to_dict(orient="records")[0]
 
-# user_data
+with st.container(
+    border=False,
+    horizontal=False,
+    horizontal_alignment="left",
+    vertical_alignment="top",
+):
+    with st.container(border=False, horizontal=True, vertical_alignment='top', height='content'):
+        with st.container(
+            border=True,
+            horizontal=False,
+            horizontal_alignment="center",
+            vertical_alignment="center",
+            width=400, 
+            height=280
+        ):
+            st.image(st.user.picture)
+            st.header(st.user["name"].title(), width="content")
+            st.caption(user_data['user_type'].title(), width='content')
 
-with st.container(border=False, horizontal=False, vertical_alignment="top"):
-    with st.container(
-        border=False,
-        horizontal=False,
-        horizontal_alignment="center",
-        vertical_alignment="top",
-    ):
-        st.image(st.user.picture)
-        st.header(st.user["name"], width="content")
+        with st.container(border=True, horizontal=False, height=280):
+            st.markdown("#### :orange[Account Details]")
 
-    with st.container(
-        border=False,
-        horizontal=False,
-        horizontal_alignment="left",
-        vertical_alignment="top",
-    ):
-        st.write(f":orange[Email]: {st.user.email}")
-        st.write(f":orange[Phone]: {user_data['contact']}")
-        st.write(
-            f":orange[Status]: {'Verified' if st.user.email_verified else 'Not Verified'}"
-        )
-        st.write(f":orange[Role]: {user_data['user_type'].title()}")
-        st.write(f":orange[Date Joined]: {user_data['created_at']}")
+            with st.container(border=False, horizontal=True):
+            # email_cols = st.columns([.8,2], gap='small')
+                st.markdown(f"**Email:**", width=120)
+                st.markdown(st.user.email)
+
+            with st.container(border=False, horizontal=True):
+                st.markdown("**Phone:**", width=120)
+                st.markdown(user_data['contact'])
+
+            with st.container(border=False, horizontal=True):
+                st.markdown("**Account Status:**", width=120)
+                st.markdown(":green[Verified]" if st.user.email_verified else 'Not Verified')
+
+            with st.container(border=False, horizontal=True):
+                st.markdown("**Telegram Status:**", width=120)
+                st.markdown(":green[Linked]" if user_data["telegram_chat_id"] else 'Not Linked')
+
+            with st.container(border=False, horizontal=True):
+                st.markdown("**Date Joined:**", width=120)
+                st.markdown(user_data['created_at'])
+    
+    with st.container(border=True):
+        st.markdown("#### :orange[Activity Overview]")
+        st.write("Coming soon: recent logins, tasks, actions…")
